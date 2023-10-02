604df6f756b12814ef1ba6dfd16cc06728dfcc3b
StillMonad <88677966+StillMonad@users.noreply.github.com>
2023-10-02 03:52:28 +0300

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -1,8 +1,8 @@
 
 cd DiscordMonitoringBot
 git pull origin main
-docker build . -t monitoring
-docker stop $(docker ps -a -q)
-docker run -d -v SharedVolume:/sharedvolume monitoring
+docker build . -t DiscordMonitor
+docker stop discord_monitor
+docker run --name discord_monitor -d -v SharedVolume:/sharedvolume DiscordMonitor
 
 # db path: sqlite3 /var/lib/docker/volumes/SharedVolume/_data/activity_log.db table ActivityLog
